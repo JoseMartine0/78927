60b9ddbfd683f9ae0a423a40e15255f8926dd52c
JoseMartine0 <zs19016416@estudiantes.uv.mx>
2024-06-25 06:21:12 -0600

    1

---
+++ src/main/java/mx/uv/dependencias/DependenciasApplication.java
@@ -0,0 +1,13 @@
+package mx.uv.dependencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/uv/dependencias/DependenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.dependencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
